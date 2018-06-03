e7d75ef8426d4dc2368db882e45a98c5a5e5ef8a
Zac Bell <bellz@byui.edu>
2018-06-02 22:05:01 -0600

    updated the code-behind
    captured text field values, calculation, output data

--- GlazerCalc/MainPage.xaml.cs
+++ GlazerCalc/MainPage.xaml.cs
@@ -25,16 +25,123 @@ namespace GlazerCalc
         public MainPage()
         {
             this.InitializeComponent();
+            //sliderOutput.Text = quantitySlider.Value.ToString();
         }
 
-        private async void Button_Click(object sender, RoutedEventArgs e)
-        {
-            MediaElement mediaElement = new MediaElement();
-            var synth = new Windows.Media.SpeechSynthesis.SpeechSynthesizer();
-            Windows.Media.SpeechSynthesis.SpeechSynthesisStream stream =
-                await synth.SynthesizeTextToStreamAsync("Hello World!");
-            mediaElement.SetSource(stream, stream.ContentType);
-            mediaElement.Play();
+
+
+        //  heightTextBox
+        //  widthTextBox
+        //  quantitySlider
+        //  colorComboBox
+        //  dateOutput 
+        //  areaOutput
+        //  lengthOutput
+
+
+        private void calcButton_Click(object sender, RoutedEventArgs e)
+        {                                   
+            if (heightTextBox.Text == "" || widthTextBox.Text == "")
+            {
+                errorMessage.Text = "Make sure your height is an Int or Double (e.g. 12 or 12.35) and that the field is not empty.";
+            } else
+            {
+                double height = Convert.ToDouble(heightTextBox.Text);
+                double width = Convert.ToDouble(widthTextBox.Text);
+                var quantity = quantitySlider.Value;
+                var color = colorComboBox.SelectedItem;
+                var woodLength = 2 * (width + height) * 3.25;
+                var glassArea = 2 * (width * height);
+                dateOutput.Text = DateTime.Now.ToString();
+                areaOutput.Text = glassArea.ToString();
+                lengthOutput.Text = woodLength.ToString();
+            }
         }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        private void heightTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+        private void widthTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+        private void quantitySlider_ValueChanged(object sender, RangeBaseValueChangedEventArgs e)
+        {
+
+        }
+        private void colorComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+        private void dateOutput_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void areaOutput_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void lengthOutput_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void sliderOutput_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void errorMessage_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
+        //private async void Button_Click(object sender, RoutedEventArgs e)
+        //{
+        //    MediaElement mediaElement = new MediaElement();
+        //    var synth = new Windows.Media.SpeechSynthesis.SpeechSynthesizer();
+        //    Windows.Media.SpeechSynthesis.SpeechSynthesisStream stream =
+        //        await synth.SynthesizeTextToStreamAsync("Hello World!");
+        //    mediaElement.SetSource(stream, stream.ContentType);
+        //    mediaElement.Play();
+        //}
     }
 }
